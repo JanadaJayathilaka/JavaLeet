05df6f59c7a57ce6dae00bc3081dedf3330a546c
janada jayathilaka <86776703+JanadaJayathilaka@users.noreply.github.com>
2025-03-10 17:25:22 +0530

    palindrome 1st sol

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,34 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
         Solution solution = new Solution();
-        int[] nums = {2,2,4,4,3};
+        System.out.println(solution.isPalindrome(-121));
 
-
-        System.out.println(solution.singleNumber(nums));
     }
 }
-
 class Solution {
-    public int singleNumber(int[] nums) {
-        int j = 1;
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
-                j++;
-            }
+    public boolean isPalindrome(int x) {
+        String xString = Integer.toString(x);
+        char[] charArray = xString.toCharArray();
+
+
+        int[] arr1 = new int[charArray.length];
+        int[] arr2 = new int[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+
+                arr1[i] = charArray[i]-'0' ;
+
+
 
         }
-        return j;
+        int k = 0;
+        for(int j = charArray.length-1;j>=0;j--){
+            arr2[k] = arr1[j];
+            k++;
+        }
+
+        return Arrays.equals(arr1,arr2);
+
+
     }
 }
