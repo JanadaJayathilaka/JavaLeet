0ec088286beca7ab41fb0106fe2a92d30ca0d262
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-04-20 20:33:27 +0530

    kunal 10 armstrom num

--- src/Solution.java
+++ src/Solution.java
@@ -4,43 +4,30 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
 
-        String msg = myGret(name);
-        System.out.println(msg);
+        System.out.println(isArmsorNot(153));
 
 
 
 
-        }
-
-        static String myGret(String name){
-        String message = "hello "+name;
-        return message;
-        }
-    static int sum2(){
-        Scanner scanner =   new Scanner(System.in);
-        System.out.print("enter num 1: ");
-        int num1= scanner.nextInt();
-        System.out.println("enter num2: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1+num2;
-        return sum;
     }
-    static void sum(){
-        Scanner scanner =   new Scanner(System.in);
-        System.out.print("enter num 1: ");
-        int num1= scanner.nextInt();
-        System.out.println("enter num2: ");
-        int num2 = scanner.nextInt();
+    static boolean isArmsorNot(int n){
+        int original = n;
+        int sum =0;
+        while(n>0){
+            int rem = n%10;
+            n=n/10;
+            sum+= rem*rem*rem;
+        }
 
-        int sum = num1+num2;
-        System.out.println("The sum = "+sum);
+        if(sum==original){
+            return true;
+        }
+        else
+            return false;
     }
 
-        }
+}
 
 
 
